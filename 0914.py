177ac506c7f703372025ecc25a9b3c21a1b5080e
changwookim <kcw8335@gmail.com>
2020-09-14 10:37:45 +0900

    0914

---
+++ 0914.py
@@ -0,0 +1,42 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12926
+# 시저 암호
+
+# <문제 설명>
+# - 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다.
+# - 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다.
+# - "z"는 1만큼 밀면 "a"가 됩니다. 
+# - 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+
+# <제한 조건>
+# - 공백은 아무리 밀어도 공백입니다.
+# - s는 알파벳 소문자, 대문자, 공백으로만 이루어져 있습니다.
+# - s의 길이는 8000이하입니다.
+# - n은 1 이상, 25 이하인 자연수입니다.
+
+def solution(s, n):
+    answer = ''
+    for i in list(s):
+        ascii_number = ord(i)
+        # 공백일 경우
+        if ascii_number == 32:
+            answer += chr(ascii_number)
+        # 대문자일 경우
+        elif 65 <= ascii_number <= 90 :
+            ascii_number += n
+            # 아스키를 n만큼 더해서 밀었는데 90초과 즉, Z보다 큰 아스키값이 나오면 순환
+            if ascii_number > 90:
+                ascii_number = ascii_number - 90 + 64
+            answer += chr(ascii_number)
+        # 소문자일 경우
+        elif 97 <= ascii_number <= 122:
+            ascii_number += n
+            # 아스키를 n만큼 더해서 밀었는데 122초과 즉, z보다 큰 아스키값이 나오면 순환
+            if ascii_number > 122:
+                ascii_number = ascii_number - 122 + 96
+            answer += chr(ascii_number)
+    return answer
+
+
+print(solution("AB", 1))
+print(solution("z", 1))
+print(solution("a B z", 4))
