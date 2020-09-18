36c4515730bbfaee18460d709e78d8db6b91b1b7
changwookim <kcw8335@gmail.com>
2020-09-18 17:30:22 +0900

    0918

---
+++ 0918.py
@@ -0,0 +1,21 @@
+# https://programmers.co.kr/learn/courses/30/lessons/68644?language=python3
+# 두 개 뽑아서 더하기
+
+# <문제 설명>
+# - 정수 배열 numbers가 주어집니다.
+# - numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 
+# - 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+def solution(numbers):
+    answer = []
+    for i in range(0, len(numbers)):
+        for j in range(i+1, len(numbers)):
+            s = numbers[i] + numbers[j]
+            if s not in answer:
+                answer.append(s)
+    answer.sort()
+    return answer
+
+
+print(solution([2,1,3,4,1]))
+print(solution([5,0,2,7]))
