bad3bf4c23793254835abeae97ea6c8f4690d5b7
KIM chang woo <kcw8335@gmail.com>
2020-09-23 00:25:01 +0900

    0923

--- 0923.py
+++ 0923.py
@@ -12,9 +12,6 @@
 # - strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
 # - 모든 strings의 원소의 길이는 n보다 큽니다.
 # - 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
-def index_ascii(n):
-    return 
-
 def solution(strings, n):
     return sorted(sorted(strings), key=lambda string: string[n])
 
