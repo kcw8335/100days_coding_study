955565aed749849b15854bfc5fe54416b25e4191
KIM chang woo <kcw8335@gmail.com>
2020-09-30 00:01:13 +0900

    0930

---
+++ 0930.py
@@ -0,0 +1,95 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42862
+# 체육복
+
+# <문제 설명>
+# - 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다.
+# - 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다.
+# - 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다.
+# - 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다.
+# - 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+# - 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 
+# - 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때,
+# - 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+
+# <제한 사항>
+# - 전체 학생의 수는 2명 이상 30명 이하입니다.
+# - 체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+# - 여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+# - 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+# - 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 
+# - 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+
+def solution(n, lost, reserve):
+    answer = 0
+    tmp = [1 for i in range(n)]
+    # 여벌의 체육복을 가지고 온 학생 조사
+    for i in reserve:
+        tmp[i-1] += 1
+    # 체육복을 잃어버린 학생 조사
+    for i in lost:
+        tmp[i-1] -= 1
+
+    # 얕은 복사와 깊은 복사의 차이를 공부했다...
+    tmp1 = list(tmp)
+
+    for i in range(n):
+        if tmp[i] == 2:
+            try:
+                if tmp[i+1] == 0:
+                    tmp[i+1] += 1
+                    tmp[i] -= 1
+            except:
+                pass
+
+    for i in range(n):
+        if tmp[i] == 2:
+            try:
+                if tmp[i-1] == 0 and (i-1) >= 0:
+                    tmp[i-1] += 1
+                    tmp[i] -= 1
+            except:
+                pass
+
+    for i in tmp:
+        if i != 0:
+            answer += 1
+
+    # =========================================================
+    for i in range(n):
+        if tmp1[i] == 2:
+            try:
+                if tmp1[i-1] == 0 and (i-1) >= 0:
+                    tmp1[i-1] += 1
+                    tmp1[i] -= 1
+            except:
+                pass
+    
+    for i in range(n):
+        if tmp1[i] == 2:
+            try:
+                if tmp1[i+1] == 0:
+                    tmp1[i+1] += 1
+                    tmp1[i] -= 1
+            except:
+                pass
+    answer1 = 0
+    for i in tmp1:
+        if i != 0:
+            answer1 += 1
+    # =========================================================
+    return max(answer, answer1)
+
+n = 5; lost = [2, 4]; reserve =  [1,3,5]
+print(solution(n, lost, reserve))
+# 정답 - 5
+
+n = 5; lost = [2, 4]; reserve = [3]
+print(solution(n, lost, reserve))
+# 정답 - 4
+
+n = 3; lost = [3]; reserve = [1]
+print(solution(n, lost, reserve))
+# 정답 - 2
+
+n = 5; lost = [1, 3]; reserve = [2, 4]
+print(solution(n, lost, reserve))
